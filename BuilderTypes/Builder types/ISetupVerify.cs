84a51aa388cb32f32f6997ebada69ae58b166cfb
Tony Hallett <tonyhallett74@gmail.com>
2021-06-22 15:38:02 +0100

    comment InSequence

--- BuilderTypes/Builder types/ISetupVerify.cs
+++ BuilderTypes/Builder types/ISetupVerify.cs
@@ -6,6 +6,8 @@ namespace MoqProtectedGenerated
     public interface ISetupVerify<TSetup,TSetupSequence>
     {
         TSetup Setup();
+        //https://github.com/moq/moq4/pull/1174
+        //TSetup InSequence(MockSequence mockSequence);
         TSetupSequence SetupSequence();
         void Verify(Times? times = null, string failMessage = null);
     }
