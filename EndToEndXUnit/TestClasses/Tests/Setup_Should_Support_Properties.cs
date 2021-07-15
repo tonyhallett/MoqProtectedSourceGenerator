7cceaf4a36a82ea6f81797059af5fe826d8c9ec2
Tony Hallett <tonyhallett74@gmail.com>
2021-07-15 12:42:51 +0000

    property tests

---
+++ EndToEndXUnit/TestClasses/Tests/Setup_Should_Support_Properties.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace EndToEndTests
+{
+    public class Setup_Should_Support_Properties : MoqProtectedSourceGeneratorTestBase
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        protected override string Source => TestSource.ProtectedInSource(
+            @"
+    public event EventHandler AnEvent;
+    protected abstract int GetSet {get;set;}
+    public void SetGetSet(int value){
+        GetSet = value;
+    }
+    public int GetGetSet(){
+        return GetSet;
+    }
+
+    protected abstract int Get {get;}
+    public int GetGet(){
+        return Get;
+    }
+
+    protected abstract int Set {set;}
+    public void SetSet(int value){
+        Set = value;
+    }
+",
+            @"
+    // get uses Moq ISetupGetter 
+    var getGetSetCalled = false;
+    mock.GetSet().Get().Build().Setup().Callback(() => getGetSetCalled = true).Returns(1);
+    Assert.AreEqual(1,mocked.GetGetSet());
+    Assert.True(getGetSetCalled);
+
+    var setGetSetCalled = false;
+    mock.GetSet().Set(1).Build().Setup().Callback(() => setGetSetCalled = true);
+    mocked.SetGetSet(1);
+    Assert.True(setGetSetCalled);
+    
+    var getSetSetValue = 0;
+    mock.GetSet().Set(1).Build().Setup().Callback( v => getSetSetValue = v).Throws(new ExpectedException());
+    Assert.Throws<ExpectedException>(()=>mocked.SetGetSet(1));
+    Assert.AreEqual(1, getSetSetValue);
+
+    IInvocation invocation = null;
+    InvocationAction invocationAction = new InvocationAction(_invocation => invocation = _invocation);
+    mock.GetSet().Set(1).Build().Setup().Callback(invocationAction);
+");
+
+        public Setup_Should_Support_Properties(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        protected override void Log(string message)
+        {
+            testOutputHelper.WriteLine(message);
+        }
+
+
+        [Fact]
+        public void Execute()
+        {
+            Test();
+        }
+    }
+}
---
+++ EndToEndXUnit/TestClasses/Tests/Should_Support_Property_Stubbing.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace EndToEndTests
+{
+    public class Should_Support_Property_Stubbing: MoqProtectedSourceGeneratorTestBase
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        protected override string Source => TestSource.ProtectedInSource(
+            @"
+    protected abstract int Property {get;set;}
+    public void SetProperty(int value){
+        Property = value;
+    }
+    public int GetProperty(){
+        return Property;
+    }
+",
+            @"
+    mock.Property().SetupProperty();
+    mocked.SetProperty(1);
+    Assert.AreEqual(1, mocked.GetProperty());
+");
+
+        public Should_Support_Property_Stubbing(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        protected override void Log(string message)
+        {
+            testOutputHelper.WriteLine(message);
+        }
+
+
+        [Fact]
+        public void Execute()
+        {
+            Test();
+        }
+    }
+}
